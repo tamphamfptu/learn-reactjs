5bffbff8210270cb9715da5da3164a583179e4ac
Nghia Nguyen <ntbnghia2308@gmail.com>
2022-03-21 23:51:32 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const name = "Tam";
+  const age = 22;
+  const isMale = true;
+  const student = {
+    name: "Easy Frontend",
+  };
+  const colorList = ["red", "green", "blue"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nghia Thuong   
+          Hello {name} - {age} - {isMale ? "Male" : "Female"}
         </p>
-        <p>dit me Tam Pham</p>
-      
+        {isMale && (
+          <div>
+            <p>Male</p>
+            <p>Male</p>
+            <p>Maleficent</p>
+          </div>
+        )}
+        {isMale && (
+          <React.Fragment>
+            <p>Male1</p>
+            <p>Male2</p>
+            <p>Male3</p>
+          </React.Fragment>
+        )}
+        <p>{student.name}</p>
       </header>
     </div>
   );
